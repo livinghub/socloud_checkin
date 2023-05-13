ff9e53706fcfcde99ad7e9164726045685887302
lyh <707884308@qq.com>
2023-05-13 20:45:06 +0800

    v0.2

--- checkin.py
+++ checkin.py
@@ -3,6 +3,29 @@ import json
 from selenium.webdriver.common.by import By
 import time
 import sys
+import platform
+import subprocess
+
+def get_driver_version():
+    system = platform.system()
+    if system == "Linux":
+        cmd = r'google-chrome --version'
+    elif system == "Darwin":
+        cmd = r'''/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version'''
+    elif system == "Windows":
+        cmd = r'''powershell -command "&{(Get-Item 'C:\Program Files\Google\Chrome\Application\chrome.exe').VersionInfo.ProductVersion}"'''
+
+    try:
+        out, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    except IndexError as e:
+        print('Check chrome version failed:{}'.format(e))
+        return 0
+    if system == "Linux" or system == "Darwin":
+        out = out.decode("utf-8").split(" ")[2].split(".")[0]
+    elif system == "Windows":
+        out = out.decode("utf-8").split(".")[0]
+    # print(out)
+    return out
 
 def socloud(cookie_string):
     # 设置驱动选项
@@ -10,8 +33,10 @@ def socloud(cookie_string):
     # options.add_argument('--proxy-server=socks5://127.0.0.1:10088')
     options.add_argument("--disable-popup-blocking")
 
+    # 获取驱动版本
+    version = get_driver_version()
     # 创建驱动
-    driver = uc.Chrome(version_main='110', options=options)
+    driver = uc.Chrome(version_main=version, options=options)
 
     # 记得写完整的url 包括http和https
     driver.get('https://socloud.me/user##')
